1648618175b0f20b049d1a69bd1a1aaf493edc94
Conte Alexandre <contealexandre3@proton.me>
2025-12-31 14:17:54 +0100

    Remove comment from AuthRequest interface
    Removed comment about token definition in AuthRequest interface.

--- src/api/types/types.ts
+++ src/api/types/types.ts
@@ -1,7 +1,6 @@
 import { Request } from "express";
 import { UserRole } from "../modules/users/users.model";
 
-// On définit ce que contient le jeton une fois décodé
 export interface AuthRequest extends Request {
   user?: {
     userId: string;
